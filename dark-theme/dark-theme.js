87d2452e652f4753e364d90acda2b3d22258e0b8
unknown <macrowell@yahoo.com>
2022-04-04 18:37:53 -0400

    created light mode and dark mode buttons that change background color and text color of page

--- dark-theme/dark-theme.js
+++ dark-theme/dark-theme.js
@@ -1,1 +1,14 @@
 // you need to create DOM selectors to grab the correct HTML elements and update their styles to the opposite theme's css classes when you want to toggle the theme off and on
+
+
+const darkMode = () => {
+    const background = document.querySelector('html');
+    background.style.color = 'white'
+    background.style.backgroundColor = 'black'
+};
+
+const lightMode = () => {
+    const background = document.querySelector('html');
+    background.style.color = 'black'
+    background.style.backgroundColor = 'white'
+};
