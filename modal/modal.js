e3eae5e95b6962b5541ed631c98fa8534b3ab7e0
unknown <macrowell@yahoo.com>
2022-04-05 19:52:19 -0400

    worked on modal page

--- modal/modal.js
+++ modal/modal.js
@@ -1,1 +1,17 @@
-// you will need to toggle the visibility off and on with DOM selectors
+const modalOn = document.getElementById('modal-button')
+const modalOff = document.getElementById('modal-close-button')
+const modalContainer = document.getElementById('modal-container')
+
+
+modalOn.addEventListener('click', () => {
+    modalContainer.classList.add('modal-on');
+});
+
+modalOff.addEventListener('click', () => {
+    modalContainer.classList.remove('modal-on');
+});
+
+
+console.log(modalOn)
+console.log(modalOff)
+console.log(modalContainer)
